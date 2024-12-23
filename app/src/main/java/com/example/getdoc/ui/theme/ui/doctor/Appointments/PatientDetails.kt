478e7cf14508a189ac47b9cc5a70c1a82396c0d6
Sagnik Deb <sagnik85842@gmail.com>
2024-12-23 20:01:42 +0600

    comment

--- app/src/main/java/com/example/getdoc/ui/theme/ui/doctor/Appointments/PatientDetails.kt
+++ app/src/main/java/com/example/getdoc/ui/theme/ui/doctor/Appointments/PatientDetails.kt
@@ -54,12 +54,7 @@ fun PatientDetailsPage() {
             Column(
                 modifier = Modifier.fillMaxWidth()
             ) {
-                PatientDetailRow(label = "Name:", value = "Sagnik Deb")
-                PatientDetailRow(label = "Gender:", value = "Male")
-                PatientDetailRow(label = "Age:", value = "32 Year Old")
-                PatientDetailRow(label = "Booking For:", value = "Self")
-                PatientDetailRow(label = "Booking ID:", value = "100068")
-                PatientDetailRow(label = "Phone No:", value = "01711111111")
+                //ekhan thaki katsi
             }
         }
 
